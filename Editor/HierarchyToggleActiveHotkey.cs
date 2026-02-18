7046a30c827482a99fa51e9ccc59a8b5387d0998
plastfw <ibanaski24@gmail.com>
2026-02-18 16:57:07 +0300

    add_hierarchyDeactivator

---
+++ Editor/HierarchyToggleActiveHotkey.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class HierarchyToggleActiveHotkey
+{
+    private const KeyCode TOGGLE_KEY = KeyCode.A;
+
+    static HierarchyToggleActiveHotkey() =>
+        EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+
+    private static void OnHierarchyGUI(int instanceId, Rect rect)
+    {
+        var e = Event.current;
+        if (e.type != EventType.KeyDown)
+            return;
+
+        if (e.keyCode != TOGGLE_KEY)
+            return;
+
+        if (!rect.Contains(e.mousePosition))
+            return;
+
+        var obj = EditorUtility.InstanceIDToObject(instanceId) as GameObject;
+        if (obj == null)
+            return;
+
+        Undo.RecordObject(obj, "Toggle Active");
+        obj.SetActive(!obj.activeSelf);
+        EditorUtility.SetDirty(obj);
+
+        e.Use();
+    }
+}
